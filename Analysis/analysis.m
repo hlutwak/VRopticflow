ac16a1e6dddc49a4581db5cb361dfaff8e29ce25
Hope Lutwak <37197337+hlutwak@users.noreply.github.com>
2023-04-10 13:55:40 -0400

    Add files via upload

--- Analysis/analysis.m
+++ Analysis/analysis.m
@@ -3,7 +3,7 @@ addpath(genpath('Data'))
 
 %% plotting HMD data
 % load oc.modelViewDataLeft
-load('test-fixed-20230322T142931-0')
+% load('test-fixed-20230322T142931-0')
 figure
 plot3(oc.HMD(3:4:end,4), oc.HMD(1:4:end,4),...
     oc.HMD(2:4:end,4))
@@ -22,11 +22,26 @@ ylabel('x')
 zlabel('y')
 title('camera view tracking')
 
-figure, plot3(pa.zPosition, pa.xPosition,ones(size(pa.xPosition))*pa.floorHeight+pa.paddleHalfHeight)
-axis equal
-xlabel('z')
-ylabel('x')
-zlabel('y')
+
+% figure, plot3(pa.zPosition, pa.xPosition,ones(size(pa.xPosition))*pa.floorHeight+pa.paddleHalfHeight)
+% axis equal
+% xlabel('z')
+% ylabel('x')
+% zlabel('y')
 
 % how much did you travel
 vecnorm(oc.modelViewDataLeft(end-3:end-1,4)-oc.modelViewDataLeft(1:3,4))
+
+%% 3D velocities tested
+
+figure, scatter(pa.fullFactorial(:,1), pa.fullFactorial(:,2)), axis equal
+
+
+%% screencap
+s = size(screenCap);
+
+figure
+for ii = 1:s(3)
+    imagesc(screenCap(:,:,ii)), colormap(gray)
+    pause(1/10)
+end
