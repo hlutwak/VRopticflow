ac45729a43f5409d02ef362bd94c2aff05b30400
Hope Lutwak <hlutwak@nyu.edu>
2024-03-13 16:21:31 -0400

    sm full 2

--- RunExperiment.m
+++ RunExperiment.m
@@ -1,7 +1,7 @@
-                                                                                                                                                                                                               % function RunExperiment   
- 
-   %  14-Dec-2015  jf Written. Der   ived from OculusSDK2PongDemo_Fixed.m
-%  6-Jan-2016  jf Edited to improve lag ged condition performance   
+                                                                                                                                                                                                                                             % function RunExperiment   
+  
+       %  14-Dec-2015  jf Written. Der   ived  from OculusSDK2PongDemo_Fixed.m
+%  6-Jan-2016  jf Edited to improve lag  ged condition performance   
 % 14-Jan-2016 jf  Switched over to Windows pla tform and optimized the code
  % for timing and stimulus presentation - including now measured gamma
  % correction  
