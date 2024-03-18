1a1c89acdae184f11722cf4e1abce0b72b450e39
Hope Lutwak <hlutwak@nyu.edu>
2024-03-18 17:18:42 -0400

    DL moncular full 2

--- SetupParameters.m
+++ SetupParameters.m
@@ -8,7 +8,7 @@ function [ds,pa] = SetupParameters(ds)
 
 %% Basic experimental specs 
 
-pa.subjectName = 'DL-monocular';
+pa.subjectName = 'DL-full';
 pa.block =2 ;
 pa.feedbackFlag = 1;  % 0 --> no feedback;  1 --> only sound-based feedback;  2 --> visual + sound-based feedback
 
