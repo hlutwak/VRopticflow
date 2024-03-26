80df86fbe50af57456fcdfab266e7f43c872979d
Hope Lutwak <hlutwak@nyu.edu>
2024-03-26 12:01:46 -0400

    IG mono 2

--- SetupParameters.m
+++ SetupParameters.m
@@ -7,7 +7,7 @@ function [ds,pa] = SetupParameters(ds)
 
 
 %% Basic experimental specs 
-pa.subjectName = 'IG-full';
+pa.subjectName = 'IG-monocular';
 pa.block = 2 ;
 pa.feedbackFlag = 1;  % 0 --> no feedback;  1 --> only sound-based feedback;  2 --> visual + sound-based feedback
 
